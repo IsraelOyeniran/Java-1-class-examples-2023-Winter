91387e84fe528d1ed52d9d947cfb8c01e6bc2f2f
JoshTaylorNL <jktylr@gmail.com>
2023-01-11 11:22:01 -0330

    Initial commit - Hello World with new Winter 2023 class!

---
+++ src/main/java/ca/nl/cna/java1/introduction/HelloWorld.java
@@ -0,0 +1,16 @@
+package ca.nl.cna.java1.introduction;
+
+/**
+ * Simple hello world application!
+ *
+ * @author Josh
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+    }
+
+}
