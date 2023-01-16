8f3a3672df4a269622533b5db81d7f4e89e8a278
Josh Taylor <33845236+joshtaylornl@users.noreply.github.com>
2023-01-16 09:27:12 -0330

    Ch2 Lecture started

---
+++ src/main/java/ca/nl/cna/java1/introduction/FunWithBasicJava.java
@@ -0,0 +1,25 @@
+package ca.nl.cna.java1.introduction;
+
+/**
+ * Playing around with some basic Java syntax
+ *
+ * @author josh.taylor
+ */
+public class FunWithBasicJava {
+
+    /**
+     * Simple main method that starts some basic java lessons
+     * @param args command arguments - none expected
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Fun with basic Java!...");
+        System.out.print("Here we go. Let's start a new line: \nand insert tab >\t< did it work?");
+        System.out.print("Newline \nCan you see a backslash: \\ \n");
+
+        String message = "Let's learn some basic Java stuff!";
+        System.out.println(message);
+
+    }
+
+}
--- src/main/java/ca/nl/cna/java1/introduction/HelloWorld.java
+++ src/main/java/ca/nl/cna/java1/introduction/HelloWorld.java
@@ -7,10 +7,30 @@ package ca.nl.cna.java1.introduction;
  */
 public class HelloWorld {
 
+    /**
+     * Simple main method that echos a message to the user
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
 
+    public static void doSomething(){
 
     }
+
+
+
+    /**
+     * This is a java doc comment
+     */
+
+    //this is a single line comment
+
+    /*
+
+    This is a multi line comment
+
+    */
 
 }
