2d843d2fda1b6d262f1b4f63184e6c17f7395e11
Josh Taylor <33845236+joshtaylornl@users.noreply.github.com>
2023-02-13 10:08:19 -0330

    Ch4 ongoing. Assignment help updated.

--- src/main/java/ca/nl/cna/java1/assignmenthelp/a1/FunWithInvoices.java
+++ src/main/java/ca/nl/cna/java1/assignmenthelp/a1/FunWithInvoices.java
@@ -1,29 +1,29 @@
 package ca.nl.cna.java1.assignmenthelp.a1;
 
 /**
- * You can use this type of class to demonstrate you invoice
+ * Use this class to create the invoice for the assignment!
  *
  * @author Josh
  */
-public class InvoiceTester {
+public class FunWithInvoices {
 
     public static void main(String[] args) {
         System.out.println("Fun with Invoices!");
 
-        //Create the products
+        //TODO change these to reflect the assignment
         Product product1 = new Product(101, "Carton of Milk", 4.99);
         Product product2 = new Product(102, "Orange Juice", 6.99);
         Product product3 = new Product(107, "Loaf of Bread", 3.99);
-
 
         InvoiceItem invoiceItem1 = new InvoiceItem(product1, 2);
         InvoiceItem invoiceItem2 = new InvoiceItem(product2, 1);
         InvoiceItem invoiceItem3 = new InvoiceItem(product3, 3);
 
+        Invoice invoice = new Invoice();
+        invoice.addInvoiceItem(invoiceItem1);
+        //TODO Finish adding invoice items (there should be 4)
 
-        //TODO Create and add to an invoice
-
-        //TODO Print the invoice showing the totals
+        printInvoice(invoice);  //TODO Finish this method
 
     }
 
@@ -32,10 +32,14 @@ public class InvoiceTester {
 
         //TODO write this method
         //TODO add a table head with ID, Description, QTY, Unit Price, Line Total
-
         //TODO make sure it's properly formatted. Use %XS to space strings
 
-        //TODO Add the invoice total
+        //TODO write a for loop on the invoiceItems (use number of items)
+        for (int i = 0; i < invoice.getNumberItems(); i++) {
+            //You can use the print format from InvoiceItem Tester but clean it up
+        }
+
+        //TODO Add the invoice total at the bottom
 
     }
 
